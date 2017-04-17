f2d70571c8fc1848ed8112caf093acff5729585f
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-18 00:04:32 +0800

    fix errors

--- albums/index.android.js
+++ albums/index.android.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import ReactNative, { AppRegistry } from 'react-native';
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
 
 const App = () => {
 	return (
