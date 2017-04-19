33074577b5d0eb99f49ac5a9f125a628ce85cc97
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-20 00:22:16 +0800

    card component

---
+++ albums/src/components/Card.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class Card extends Component {
+	render() {
+		return (
+			<View></View>
+		);
+	}
+}
+
+export default Card;
