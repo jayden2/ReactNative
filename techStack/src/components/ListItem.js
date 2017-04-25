97f0feffa6545613b7e7ffa57b22771fef41086c
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-26 02:43:16 +0800

    animation for componentWillUpdate lifecycle method

--- techStack/src/components/ListItem.js
+++ techStack/src/components/ListItem.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react'
-import { Text, TouchableWithoutFeedback, View } from 'react-native'
+import { Text, TouchableWithoutFeedback, LayoutAnimation, UIManager, View } from 'react-native'
 import { connect } from 'react-redux'
 import { CardSection } from './common'
 import * as actions from '../actions'
 
 class ListItem extends Component {
+	componentWillUpdate() {
+		UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+		LayoutAnimation.spring()
+	}
+
 	renderDescription() {
 		const { library, expanded } = this.props
 
 		if (expanded) {
 			return (
 				<CardSection>
-					<Text>{ library.description }</Text>
+					<Text style={styles.descriptionStyle}>{ library.description }</Text>
 				</CardSection>
 			)
 		}
@@ -42,6 +47,11 @@ const styles = {
 	titleStyle: {
 		fontSize: 18,
 		paddingLeft: 15
+	},
+	descriptionStyle: {
+		flex: 1,
+		paddingLeft: 16,
+		paddingRight: 16
 	}
 }
 
