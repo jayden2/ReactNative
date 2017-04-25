aa26392be3a838ab63399fcb9e1b60f3bd4f80b7
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-26 03:10:53 +0800

    firebase, default reducer setup

--- manager/src/app.js
+++ manager/src/app.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducers from './reducers'
+import firebase from 'firebase'
+import firebaseConfig from '../firebase.json'
 
 class App extends Component {
+	componentWillMount() {
+		firebase.initializeApp(firebaseConfig)
+	}
+
 	render() {
 		return (
-			<Provider store={createStore(reducer)}>
+			<Provider store={createStore(reducers)}>
 				<View>
 					<Text>App</Text>
 				</View>
