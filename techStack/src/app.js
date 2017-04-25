4a6b8177e3be27572e1a50c2f4f20e80d2a332c5
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-25 23:13:20 +0800

    set up boilerplate for reducer

--- techStack/src/app.js
+++ techStack/src/app.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import { View, Text } from 'react-native'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import reducers from './reducers'
+import { Header } from './components/common'
 
 class App extends Component {
 	render() {
 		return (
-			<View>
-				<Text>Test</Text>
-			</View>
+			<Provider store={createStore(reducers)}>
+				<View>
+					<Text>Test</Text>
+				</View>
+			</Provider>
 		)
 	}
 }
