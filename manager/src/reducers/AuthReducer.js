8e882b6de71bcb88bd3653130749e90e5a941a56
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-29 00:21:36 +0800

    spread initial state instead of individual as its all reset

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -21,7 +21,7 @@ export default (state = INITIAL_STATE, action) => {
 			return { ...state, loading: true, error: '' }
 
 		case LOGIN_USER_SUCCESS:
-			return { ...state, user: action.payload, loading: false, error: '', email: '', password: '' }
+			return { ...state, ...INITIAL_STATE, user: action.payload }
 
 		case LOGIN_USER_FAIL:
 			return { ...state, error: 'Authentication Failed.', loading: false, password: '' }
