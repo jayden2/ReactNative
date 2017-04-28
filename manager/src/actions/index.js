7b0fbd0a7cdc82fcb50ec988e6af52476a057386
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-28 22:59:29 +0800

    user creation handling

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase'
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './constants'
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './constants'
 
 export const emailChanged = (text) => {
 	return {
@@ -18,11 +18,25 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
 	return (dispatch) => {
 		firebase.auth().signInWithEmailAndPassword(email, password)
-		.then(user => {
-			dispatch({
-				type: LOGIN_USER_SUCCESS,
-				payload: user
-			})
+		.then(user => loginUserSuccess(dispatch, user))
+		.catch(() => {
+			firebase.auth().createUserWithEmailAndPassword(email, password)
+				.then(user => loginUserSuccess(dispatch, user))
+				.catch(() => loginUserFail(dispatch))
 		})
 	}
+}
+
+const loginUserFail = (dispatch) => {
+	dispatch({
+		type: LOGIN_USER_FAIL,
+		payload: null
+	})
+}
+
+const loginUserSuccess = (dispatch, user) => {
+	dispatch({
+		type: LOGIN_USER_SUCCESS,
+		payload: user
+	})
 }
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,17 +1,22 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from '../actions/constants'
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from '../actions/constants'
 
 const INITIAL_STATE = {
 	email: '',
-	password: ''
+	password: '',
+	user: {}
 }
 
 export default (state = INITIAL_STATE, action) => {
+
 	switch (action.type) {
 		case EMAIL_CHANGED:
 			return { ...state, email: action.payload }
 		
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload }
+
+		case LOGIN_USER_SUCCESS:
+			return { ...state, user: action.payload }
 
 		default:
 			return state
