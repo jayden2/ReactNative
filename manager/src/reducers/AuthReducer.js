0af823c8fcd8810512b7b1bbb64818b4dc60b1f8
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-29 00:19:22 +0800

    login loading spinner logic, reset email, password on success

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,14 +1,14 @@
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/constants'
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from '../actions/constants'
 
 const INITIAL_STATE = {
 	email: '',
 	password: '',
 	user: null,
-	error: ''
+	error: '',
+	loading: false
 }
 
 export default (state = INITIAL_STATE, action) => {
-	console.log(action)
 
 	switch (action.type) {
 		case EMAIL_CHANGED:
@@ -17,11 +17,14 @@ export default (state = INITIAL_STATE, action) => {
 		case PASSWORD_CHANGED:
 			return { ...state, password: action.payload }
 
+		case LOGIN_USER:
+			return { ...state, loading: true, error: '' }
+
 		case LOGIN_USER_SUCCESS:
-			return { ...state, user: action.payload, error: '' }
+			return { ...state, user: action.payload, loading: false, error: '', email: '', password: '' }
 
 		case LOGIN_USER_FAIL:
-			return { ...state, error: 'Authentication Failed.', password: '' }
+			return { ...state, error: 'Authentication Failed.', loading: false, password: '' }
 
 		default:
 			return state
