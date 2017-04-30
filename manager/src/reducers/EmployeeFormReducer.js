2377953864d42c323a9437c090cacfcf6523bf91
Jayden Spencer <jaydenspencer1@gmail.com>
2017-04-30 23:43:15 +0800

    basic employee form reducer logic

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -1,11 +1,17 @@
 import { EMPLOYEE_UPDATE } from '../actions/constants'
 
 const INITIAL_STATE = {
-
+	name: '',
+	phone: '',
+	shift: ''
 }
 
 export default (state = INITIAL_STATE, action) => {
 	switch (action.type) {
+		case EMPLOYEE_UPDATE:
+			//action.payload === {prop: 'name', value: 'jimmy' }
+			return { ...state, [action.payload.prop]: action.payload.value }
+
 		default:
 			return state
 	}
