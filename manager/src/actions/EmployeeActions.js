8465b8b067c2a436778fcd08a54ca32cea3b1ccc
Jayden Spencer <jaydenspencer1@gmail.com>
2017-05-02 23:24:01 +0800

    employee delete action

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,6 +1,6 @@
 import firebase from 'firebase'
 import { Actions } from 'react-native-router-flux'
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_SAVE_SUCCESS, EMPLOYEES_FETCH_SUCCESS } from './constants'
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEE_SAVE_SUCCESS, EMPLOYEES_FETCH_SUCCESS, EMPLOYEE_DELETE_SUCCESS } from './constants'
 
 export const employeeUpdate = ({ prop, value }) => {
 	return {
@@ -41,6 +41,19 @@ export const employeeSave = ({ name, phone, shift, uid }) => {
 			.set({ name, phone, shift })
 			.then(() => {
 				dispatch({ type: EMPLOYEE_SAVE_SUCCESS })
+				Actions.employeeList({ type: 'reset' })
+			})
+	}
+}
+
+export const employeeDelete = ({ uid }) => {
+	const { currentUser } = firebase.auth()
+
+	return (dispatch) => {
+		firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
+			.remove()
+			.then(() => {
+				dispatch({ type: EMPLOYEE_DELETE_SUCCESS })
 				Actions.employeeList({ type: 'reset' })
 			})
 	}
