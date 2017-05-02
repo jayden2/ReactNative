22de39ee5ea0ad585baf2b62bf0ab1f17c63658b
Jayden Spencer <jaydenspencer1@gmail.com>
2017-05-02 18:46:08 +0800

    create modal component

---
+++ manager/src/components/common/Modal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Modal, Text, View } from 'react-native'
+import { CardSection } from './CardSection'
+import { Button } from './Button'
+
+class Confirm extends Component {
+	render() {
+		return (
+			<View>
+			</View>
+		)
+	}
+}
+
+export default Confirm
